069a8b08bba26da75c14c8be99fbbdea578fac47
Amir Nourinia <amir.nouri.nia@gmail.com>
2023-05-01 04:10:32 +0200

    working example

--- conan/Dependencies.cmake
+++ conan/Dependencies.cmake
@@ -1,1 +1,2 @@
 find_package(Boost REQUIRED COMPONENTS system)
+include_directories(${Boost_INCLUDE_DIRS})
--- src/intro2algorithms/Lesson1/Lesson1.cc
+++ src/intro2algorithms/Lesson1/Lesson1.cc
@@ -1,5 +1,11 @@
+#include "boost/uuid/random_generator.hpp"
+#include <__algorithm/ranges_for_each.h>
+#include <__ranges/transform_view.h>
 #include <algorithm>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
 #include <iostream>
+#include <optional>
 #include <ostream>
 #include <ranges>
 #include <stdlib.h>
@@ -13,15 +19,28 @@ struct Date {
   int year{0};
 
   friend std::ostream &operator<<(std::ostream &o, Date const &date);
+  bool operator==(Date const &rhs) const noexcept {
+    return (this->day == rhs.day && this->month == rhs.month &&
+            this->year == rhs.year);
+  }
 };
 
 std::ostream &operator<<(std::ostream &o, Date const &date) {
   return (o << date.day << "." << date.month << "." << date.year);
 }
 
-struct Student {
+class Student {
+private:
+  using uuid = boost::uuids::uuid;
+  using rg = boost::uuids::random_generator;
+  const uuid m_uuid;
+
+public:
+  Student(std::string &&name, Date &&date) noexcept
+      : m_uuid(rg()()), name{name}, birthday{date} {};
   std::string name;
   Date birthday;
+  uuid const &get_id() const { return this->m_uuid; }
 
   friend std::ostream &operator<<(std::ostream &o, Student const &student);
 };
@@ -36,26 +55,49 @@ private:
   std::vector<T> m_records;
 
 public:
-  Record(std::vector<T> &&records);
-  Record(std::vector<T> &records);
+  Record(std::vector<T> &&records) : m_records{records} {};
+  Record(std::vector<T> &records) : m_records{records} {};
 
-  virtual std::vector<T> find_matches() const;
+  [[nodiscard]] virtual auto find_matches() const
+      -> std::optional<std::vector<T>>;
 
   virtual ~Record() = default;
 };
 
-template <class T> std::vector<T> Record<T>::find_matches() const {
-  return (this->m_records | std::views::);
+template <>
+auto Record<Student>::find_matches() const
+    -> std::optional<std::vector<Student>> {
+  std::vector<Student> matches{};
+  matches.reserve(this->m_records.size());
+
+  std::ranges::for_each(this->m_records, [&](Student const &item) {
+    std::ranges::for_each(this->m_records, [&](Student const &otherItem) {
+      if (item.get_id() != otherItem.get_id() &&
+          item.birthday == otherItem.birthday) {
+        matches.push_back(item);
+        matches.push_back(otherItem);
+      }
+    });
+  });
+  if (!matches.empty()) {
+    return std::move(matches);
+  } else {
+    return {};
+  }
 }
 
 int main() {
-  auto students =
-      std::vector<Student>{{"Amir", {9, 4, 1992}}, {"Shiva", {19, 07, 1994}}};
+  auto students = std::vector<Student>{{"Amir", {9, 4, 1992}},
+                                       {"Shiva", {19, 07, 1994}},
+                                       {"SomeOne", {9, 4, 1992}}};
   auto records = Record<Student>(students);
-  auto matches = records.find_matches();
-
-  for (const auto &student : matches) {
-    std::cout << student << std::endl;
+  const auto matches = records.find_matches();
+  if (matches.has_value()) {
+    std::ranges::for_each(matches.value(), [](auto const &item) {
+      std::cout << item << std::endl;
+    });
+  } else {
+    std::cout << "No Matches!" << std::endl;
   }
   return 1;
 }
