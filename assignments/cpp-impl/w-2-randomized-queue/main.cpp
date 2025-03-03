e2e9a18686e62c6256d7d33f9a3eead26951cb46
Amir Nourinia <amir.nouri.nia@gmail.com>
2025-03-03 23:37:19 +0100

    Move assignments implementations to a separate module (private)

--- assignments/cpp-impl/w-2-randomized-queue/main.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2025 by Amir Nourinia
-
-import DSA;
-
-#include <cstdlib>
-int main() {
-    return EXIT_SUCCESS;
-}
--- assignments/cpp-impl/w-4-eight-puzzle/main.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2025 by Amir Nourinia
-#include <cstdlib>
-
-import DSA;
-
-int main() {
-    return EXIT_SUCCESS;
-}
